838170fddfc95d264b4f4bf6a004d9dc76271735
skive <shixiaohui@ec.com.cn>
2022-11-16 15:01:43 +0800

    add aaa

---
+++ temp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         autoRead
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       skive
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('aaa')
+})();
