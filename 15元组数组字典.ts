a86bd16884fa08cdab87781c4edf667d685c0d9b
xhoo s <1319235442@qq.com>
2023-02-20 17:09:09 +0800

    元组

---
+++ 15元组数组字典.ts
@@ -0,0 +1,17 @@
+//元组
+let hero:[string,number]=['chaoren',100];
+hero[0]='bianfuxia';
+
+let arry1:number[] =[2,3,4];
+let arry2:Array
+//数组
+
+
+//字典
+ let dic:{[key:string]:string} = {
+    'name':'xiaohu',
+    'name2':'liyao'
+ }
+
+ //回调
+  
